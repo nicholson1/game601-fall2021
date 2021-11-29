f588a702ab9f78c85114e35ce008932f9f6f1452
Neil Nicholson <48965694+nicholson1@users.noreply.github.com>
2021-11-28 23:06:11 -0500

    random changing hella trippy

---
+++ New Unity Project/Assets/_FINAL/_scripts/RandomScale.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScale : MonoBehaviour
+{
+    private float timeToChange;
+    public bool changing;
+    private float targetScale;
+    void Start()
+    {
+        timeToChange = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!changing)
+        {
+            timeToChange -= Time.deltaTime;
+        
+            if (timeToChange < 0)
+            {
+                changing = true;
+                targetScale = Random.Range(0.75f, 1.25f);
+            } 
+        }
+        else
+        {
+            transform.localScale = Vector3.MoveTowards(transform.localScale, new Vector3(targetScale, targetScale, 0), .1f * Time.deltaTime );
+            if (Vector3.Distance(transform.localScale, new Vector3(targetScale, targetScale, 0)) < .01)
+            {
+                changing = false;
+                timeToChange = Random.Range(1, 5);
+
+            }
+        }
+        
+    }
+}
